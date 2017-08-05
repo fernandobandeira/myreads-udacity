4cb44c289b8ef59a6d0d59825a0278db6312907c
Fernando Henrique Bandeira <fernandobandeira@MacBook-Pro-de-Fwenando.local>
2017-08-05 16:05:22 -0300

    Search books

--- src/App.js
+++ src/App.js
@@ -5,82 +5,86 @@ import './App.css'
 import {Link, Route} from "react-router-dom";
 
 class BooksApp extends React.Component {
-  state = {
-    books: []
-  }
+    state = {
+        books: [],
+        booksFound: []
+    }
 
-  componentDidMount() {
-    this.getBooks()
-  }
+    componentDidMount() {
+        this.getBooks()
+    }
 
-  getBooks() {
-      BooksAPI.getAll().then((books) => this.setState({ books }) )
-  }
+    getBooks() {
+        BooksAPI.getAll().then(books => this.setState({books}))
+    }
 
-  filterBooksByShelf(shelf) {
-      return this.state.books.filter(book => book.shelf === shelf)
-  }
+    searchBooks(query) {
+        if (query === '') {
+            this.setState({booksFound: []})
+            return
+        }
+        BooksAPI.search(query).then(booksFound => this.setState({booksFound}))
+    }
 
-  render() {
-    return (
-      <div className="app">
-        <Route path="/search" render={() => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link className="close-search" to="/">Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
-                NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                You can find these search terms here:
-                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+    filterBooksByShelf(shelf) {
+        return this.state.books.filter(book => book.shelf === shelf)
+    }
 
-                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                you don't find a specific author or title. Every search is limited by search terms.
-              */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
+    render() {
+        return (
+            <div className="app">
+                <Route path="/search" render={() => {
+                    this.searchBooks()
+                    return (
+                        <div className="search-books">
+                            <div className="search-books-bar">
+                                <Link className="close-search" to="/">Close</Link>
+                                <div className="search-books-input-wrapper">
+                                    <input type="text" placeholder="Search by title or author"
+                                           onChange={event => this.searchBooks(event.target.value)}/>
+                                </div>
+                            </div>
+                            <div className="search-books-results">
+                                <ListBooks books={this.state.booksFound}/>
+                            </div>
+                        </div>
+                    )
+                }}/>
+                <Route path="/" exact render={() => (
+                    <div className="list-books">
+                        <div className="list-books-title">
+                            <h1>MyReads</h1>
+                        </div>
+                        <div className="list-books-content">
+                            <div>
+                                <div className="bookshelf">
+                                    <h2 className="bookshelf-title">Currently Reading</h2>
+                                    <div className="bookshelf-books">
+                                        <ListBooks books={this.filterBooksByShelf('currentlyReading')}/>
+                                    </div>
+                                </div>
+                                <div className="bookshelf">
+                                    <h2 className="bookshelf-title">Want to Read</h2>
+                                    <div className="bookshelf-books">
+                                        <ListBooks books={this.filterBooksByShelf('wantToRead')}/>
+                                    </div>
+                                </div>
+                                <div className="bookshelf">
+                                    <h2 className="bookshelf-title">Read</h2>
+                                    <div className="bookshelf-books">
+                                        <ListBooks books={this.filterBooksByShelf('read')}/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="open-search">
+                            <Link to="/search">Add a book</Link>
+                        </div>
+                    </div>
+                )}/>
             </div>
-            <div className="search-books-results">
-              <ListBooks books={this.state.books}/>
-            </div>
-          </div>
-        )}/>
-        <Route path="/" exact render={() => (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                      <ListBooks books={this.filterBooksByShelf('currentlyReading')}/>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                      <ListBooks books={this.filterBooksByShelf('wantToRead')}/>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                      <ListBooks books={this.filterBooksByShelf('read')}/>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to="/search">Add a book</Link>
-            </div>
-          </div>
-        )}/>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default BooksApp
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 class ListBooks extends Component {
     render() {
@@ -8,23 +8,27 @@ class ListBooks extends Component {
                     <li key={book.id}>
                         <div className="book">
                             <div className="book-top">
-                                <div className="book-cover" style={{
-                                    width: 128,
-                                    height: 193,
-                                    backgroundImage: `url("${book.imageLinks.thumbnail}")`
-                                }}/>
+                                {(book.imageLinks !== undefined &&
+                                    <div className="book-cover" style={{
+                                        width: 128,
+                                        height: 193,
+                                        backgroundImage: `url("${book.imageLinks.thumbnail}")`
+                                    }}/>
+                                )}
                                 <div className="book-shelf-changer">
-                                    <select>
-                                        <option value="none" disabled>Move to...</option>
-                                        <option value="currentlyReading" selected={book.shelf === 'currentlyReading'}>Currently Reading</option>
-                                        <option value="wantToRead" selected={book.shelf === 'wantToRead'}>Want to Read</option>
-                                        <option value="read" selected={book.shelf === 'read'}>Read</option>
-                                        <option value="none" selected={book.shelf === 'none'}>None</option>
+                                    <select value={book.shelf}>
+                                        <option disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
                                     </select>
                                 </div>
                             </div>
                             <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.authors.join(', ')}</div>
+                            {(book.authors !== undefined &&
+                                <div className="book-authors">{book.authors.join(', ')}</div>
+                            )}
                         </div>
                     </li>
                 ))}
