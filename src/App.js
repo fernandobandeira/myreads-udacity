7abb54166a343f4b47d2c2bd469643db4f214760
Fernando Henrique Bandeira <fernandobandeira@MacBook-Pro-de-Fwenando.local>
2017-08-18 00:04:56 -0300

    Split functionality across components

--- src/App.js
+++ src/App.js
@@ -1,112 +1,63 @@
-import React from 'react'
-import ListBooks from './ListBooks'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
-import {Link, Route} from 'react-router-dom'
+import React from 'react';
+import * as BooksAPI from './BooksAPI';
+import './App.css';
+import { Route } from 'react-router-dom';
 
-const debounce = require('lodash.debounce');
+import SearchPage from './pages/SearchPage';
+import HomePage from './pages/HomePage';
 
 class BooksApp extends React.Component {
-    state = {
-        books: [],
-        booksFound: []
-    }
+  constructor() {
+    super();
 
-    componentDidMount() {
-        this.getBooks()
-    }
+    this.state = {
+      books: [],
+    };
 
-    getBooks() {
-        // TODO Separar em tres variaveis e chamar apenas no inicio o getAll, alem disso usar o shelf do getAll no lugar do valor do search
-        BooksAPI.getAll().then(books => this.setState({books}))
-    }
+    this.filterBooks = this.filterBooks.bind(this);
+    this.changeBookShelf = this.changeBookShelf.bind(this);
+  }
 
-    debouncedGetBooks = debounce(this.getBooks)
+  componentWillMount() {
+    BooksAPI.getAll().then(books => this.setState({ books }));
+  }
 
-    searchBooks(query) {
-        if (query === '') {
-            return this.clearBooksFound()
-        }
-        BooksAPI.search(query).then(booksFound => {
-            booksFound = booksFound.error === 'empty query' ? [] : booksFound
-            this.setState({booksFound})
-        })
-    }
+  filterBooks(books, value, prop = 'shelf') {
+    return books.filter(book => book[prop] === value);
+  }
 
-    debouncedSearchBooks = debounce(this.searchBooks)
+  changeBookShelf(book, shelf) {
+    BooksAPI.update(book, shelf).then(() => {
+      if (book.shelf !== shelf) {
+        BooksAPI.update(book, shelf).then(() => {
+          book.shelf = shelf;
 
-    clearBooksFound() {
-        this.setState({booksFound: []})
-    }
+          this.setState(state => ({
+            books: state.books.filter(b => b.id !== book.id).concat([book]),
+          }));
+        });
+      }
+    });
+  }
 
-    filterBooks(books, value, prop = 'shelf') {
-        return books.filter(book => book[prop] === value)
-    }
-
-    changeBookShelf(book, shelf) {
-        BooksAPI.update(book, shelf).then(() => this.debouncedGetBooks())
-    }
-
-    render() {
-        return (
-            <div className="app">
-                <Route path="/search" render={() => (
-                    <div className="search-books">
-                        <div className="search-books-bar">
-                            <Link className="close-search" to="/">Close</Link>
-                            <div className="search-books-input-wrapper">
-                                <input type="text" placeholder="Search by title or author"
-                                       onChange={event => this.debouncedSearchBooks(event.target.value)}/>
-                            </div>
-                        </div>
-                        <div className="search-books-results">
-                            <ListBooks books={this.state.booksFound}
-                                       onChangeBookShelf={(book, shelf) => this.changeBookShelf(book, shelf)}
-                                       getBookShelf={(book) => {
-                                           let b = this.filterBooks(this.state.books, book.id, 'id')
-                                           return b.length > 0 ? b[0].shelf : 'none'
-                                       }}/>
-                        </div>
-                    </div>
-                )}/>
-                <Route path="/" exact render={() => (
-                    <div className="list-books">
-                        <div className="list-books-title">
-                            <h1>MyReads</h1>
-                        </div>
-                        <div className="list-books-content">
-                            <div>
-                                <div className="bookshelf">
-                                    <h2 className="bookshelf-title">Currently Reading</h2>
-                                    <div className="bookshelf-books">
-                                        <ListBooks books={this.filterBooks(this.state.books, 'currentlyReading')}
-                                                   onChangeBookShelf={(book, shelf) => this.changeBookShelf(book, shelf)}/>
-                                    </div>
-                                </div>
-                                <div className="bookshelf">
-                                    <h2 className="bookshelf-title">Want to Read</h2>
-                                    <div className="bookshelf-books">
-                                        <ListBooks books={this.filterBooks(this.state.books, 'wantToRead')}
-                                                   onChangeBookShelf={(book, shelf) => this.changeBookShelf(book, shelf)}/>
-                                    </div>
-                                </div>
-                                <div className="bookshelf">
-                                    <h2 className="bookshelf-title">Read</h2>
-                                    <div className="bookshelf-books">
-                                        <ListBooks books={this.filterBooks(this.state.books, 'read')}
-                                                   onChangeBookShelf={(book, shelf) => this.changeBookShelf(book, shelf)}/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="open-search">
-                            <Link to="/search" onClick={() => this.clearBooksFound()}>Add a book</Link>
-                        </div>
-                    </div>
-                )}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="app">
+        <Route path="/search" render={() => (
+          <SearchPage books={this.state.books}
+                      filterBooks={this.filterBooks}
+                      changeBookShelf={this.changeBookShelf}
+          />
+        )}/>
+        <Route path="/" exact render={() => (
+          <HomePage books={this.state.books}
+                    filterBooks={this.filterBooks}
+                    changeBookShelf={this.changeBookShelf}
+          />
+        )}/>
+      </div>
+    );
+  }
 }
 
-export default BooksApp
+export default BooksApp;
